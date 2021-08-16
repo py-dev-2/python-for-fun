080133cbeff8ac122d3e602da8958c4448a2dc65
py-dev-2 <89005790+py-dev-2@users.noreply.github.com>
2021-08-16 12:39:31 +0530

    print.py(first project made 4 months ago)

---
+++ print.py
@@ -0,0 +1,13 @@
+#print , i learnt this on my first day , this project was made 3 days  after i started learning python...
+#it was quite easy to make , thats the reason im sharing it here
+
+print("Zoro:Hello what's you name?")
+name = input("You:")
+print(" Zoro:whats your age?")
+age = input("You:")
+print("Zoro:where do you live?")
+live = input("You:")
+print("oh , so you are "+name)
+print("your'e "+age)
+print("so , you live in "+live)
+print(".....")
